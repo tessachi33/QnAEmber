e669deeb9661af1be0bbb4e2ba91430f6d7a6afc
Tessa <Tessachi33@yahoo.com>
2015-10-02 13:03:00 -0700

    add question-tile. WIP

---
+++ app/components/question-tile.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+});
+
+// delete(question) {
+//       if (confirm('Are you sure you want to delete this question?')) {
+//         this.sendAction('destroyQuestion', question);
+//       }
+//     }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,14 @@
+import Ember from 'ember';
+
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
